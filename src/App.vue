4c95c7e6f4e0cbebb877ee02ff03b366a9825736
hai.liting <hailiting@example.com>
2020-11-18 09:44:28 +0800

    暂存

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="first commit"/>
+    <HelloWorld msg="firstddd commit"/>
   </div>
 </template>
 
